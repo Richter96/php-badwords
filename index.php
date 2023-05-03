e8a890c3dc3ee07cf9aa63ad1f77c5740ed010ca
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-03 16:31:18 +0200

    add input type text with btn submit

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
-<?php 
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+<header>
+    <h1>Words PHP</h1>
+    <div class="card-body">
+        <form action="script.php" method="GET">
+            <label for="name">Nome</label>
+            <input type="text" name="nome">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</header>   
+    
+</body>
+</html>
