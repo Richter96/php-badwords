418dda5469fe3e5caa9d8724797171d63751a377
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-03 17:28:58 +0200

    add structure html and paragraph

--- script.php
+++ script.php
@@ -1,14 +1,65 @@
 <?php
 
-$name = $_GET['nome'];
+$name = $_POST['nome'];
 
-$paragraph = $_GET["paragraph"];
-$badword = $_GET["badword"];
+$paragraph = $_POST["paragraph"];
 
-echo $name;
-echo $badword;
+$badword = $_POST["badword"];
 
-echo $paragraph;
+// echo $name;
+// echo $badword;
+// echo $paragraph;
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>SubmitData</title>
+</head>
+<body>
+    <header class="container-ld p-4 mb-4">
+        <div class="d-flex justify-content-between">
+            <h1>Words PHP</h1>
+            <h2>Ciao <?php echo $name ?></h2>
+        </div>
+    </header>
+
+    <!-- / header -->
+    <main>
+        <div class="container-md">
+            <div class="row row-col-2">
+                <div class="col d-flex justify-content-center">
+                    <!-- card original paragraph -->
+                    <div class="card" style="width:25rem;" >
+                        <div class="card-body">
+                            <h5 class="card-title">Original Paragraph</h5>
+                            <span>Lunghezza del paragrafo: <?php echo strlen($paragraph); ?></span>
+                            <p class="card-text border"><?php echo $paragraph; ?></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col d-flex justify-content-center">
+                    <!-- card censored paragraph -->
+                    <div class="card" style="width:25rem;" >
+                        <div class="card-body">
+                            <h5 class="card-title">Censored Paragraph</h5>
+                            <span>Lunghezza del paragrafo: <?php echo strlen($paragraph); ?></span>
+                            <p class="card-text border"><?php echo str_replace($badword, "***", $paragraph); ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </main>
+    
+</body>
+</html>
 
